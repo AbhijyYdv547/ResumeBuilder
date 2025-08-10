0684dc50210fca380f66e84acaa4df0b469d7215
Abhijay Yadav <yadavabhijay@gmail.com>
2025-08-10 14:54:00 +0530

    feat: add profile section

--- backend/src/models/Template.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const TemplateSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  htmlStructure: { type: String, required: true }, 
-  cssStyles: { type: String }, 
-});
-
-export default mongoose.model("Template", TemplateSchema);
